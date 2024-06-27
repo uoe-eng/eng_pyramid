10e64a7782890b7ed251e84e38e8e4b8defa241f
Colin Higgs <colin.higgs@ed.ac.uk>
2024-06-27 16:15:16 +0100

    add support utils for test databases

--- src/eng_pyramid/database.py
+++ src/eng_pyramid/database.py
@@ -1,8 +1,12 @@
 import inspect
 import re
+import testing.postgresql
+import urllib.parse
+
 from collections import (
     namedtuple,
 )
+from dataclasses import dataclass, field
 from sqlalchemy import (
     Column,
     ForeignKey,
@@ -12,8 +16,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import (
     relationship,
     backref,
+    Session,
 )
 from sqlalchemy.dialects.postgresql import UUID
+from typing import Protocol
+from warnings import warn
 
 IdType = UUID(as_uuid=True)
 
@@ -38,6 +45,7 @@ AssocDescriptor = namedtuple('AssocDescriptor', 'table singular')
 
 def camel_case(s):
     return re.sub(r'[_-]+', ' ', s).title().replace(' ', '')
+
 
 def assoc_relationship(ao_table_name, left, right, base=None, mixins=tuple(), namespace=None, *args, **kwargs):
     if not namespace:
@@ -71,3 +79,97 @@ def assoc_relationship(ao_table_name, left, right, base=None, mixins=tuple(), na
         }
     )
     return association_proxy(ao_table_name, right.singular, *args, **kwargs)
+
+
+def setup_userspace_db(settings):
+    db_url = urllib.parse.urlparse(settings.get('sqlalchemy.url'))
+    base_dir = settings.get('testing.postgresql.base_dir')
+    print('Setting up database in {}.'.format(base_dir))
+    db = testing.postgresql.Postgresql(
+        base_dir=base_dir,
+        port=db_url.port,
+    )
+    print('Database serving on {}.'.format(db.url()))
+    return db
+
+
+def add_db_parser_args(parser):
+    parser.add_argument(
+        '--destroy',
+        action='store_true',
+        help='drop_all tables before recreating.'
+    )
+    parser.add_argument(
+        '-w', '--wait',
+        action='store_true',
+        help='Wait for a keypress before exiting. Keep DB service running.'
+    )
+    parser.add_argument(
+        '--test-corner',
+        action='store_true',
+        help='Add corner case test cases to database.'
+    )
+    parser.add_argument(
+        '--test-bulk',
+        action='store_true',
+        help='Add bulk test cases to database.'
+    )
+    parser.add_argument(
+        '-t', '--test-data',
+        action='store_true',
+        help='Add test data to the database.'
+    )
+
+
+class DataCreator(Protocol):
+
+    def _add_initial(self):
+        ...
+
+    def _add_test_corner(self):
+        ...
+
+    def _add_test_bulk(self):
+        ...
+
+
+@dataclass
+class DataFactory(DataCreator):
+    dbsession: Session
+    default_n: int = 250
+    children: list['DataFactory'] = field(default_factory=list)
+
+    def add_initial(self):
+        for child in self.children:
+            child.add_initial()
+        self._add_initial()
+
+    def add_test_corner(self):
+        for child in self.children:
+            child.add_test_corner()
+        self._add_test_corner()
+
+    def add_test_bulk(self):
+        for child in self.children:
+            child.add_test_bulk()
+        self._add_test_bulk()
+
+    def _add_initial(self):
+        warn(f"_add_initial not implemented for {self.__class__}")
+
+    def _add_test_corner(self):
+        warn(f"_add_test_corner not implemented for {self.__class__}")
+
+    def _add_test_bulk(self):
+        warn(f"_add_test_bulk not implemented for {self.__class__}")
+
+
+def add_test_data(args, factory):
+    if args.test_data:
+        args.test_corner = True
+        args.test_bulk = True
+    factory.add_initial()
+    if args.test_corner:
+        factory.add_test_corner()
+    if args.test_bulk:
+        factory.add_test_bulk()
